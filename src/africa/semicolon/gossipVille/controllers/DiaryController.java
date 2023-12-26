2b516db4aee01919f91e8e4a9ae6a3ad022951ff
Joy Joseph <joy828545@gmail.com>
2023-12-26 22:12:45 +0100

    changes made

--- src/africa/semicolon/gossipVille/controllers/DiaryController.java
+++ src/africa/semicolon/gossipVille/controllers/DiaryController.java
@@ -75,4 +75,5 @@ public class DiaryController {
             return ex.getMessage();
         }
     }
+
 }
--- src/africa/semicolon/gossipVille/data/repositories/DiaryRepository.java
+++ src/africa/semicolon/gossipVille/data/repositories/DiaryRepository.java
@@ -3,8 +3,6 @@ package africa.semicolon.gossipVille.data.repositories;
 import africa.semicolon.gossipVille.data.models.Diary;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface DiaryRepository extends MongoRepository<Diary, String> {
     Diary findDiaryByUsername(String username);
 
